a46d9a2f87c7c193dd1de17e97bd96c95b946ec4
Felipe Elia <felipe.elia@get10up.com>
2024-08-08 14:28:11 -0300

    Wait for the api call

--- tests/cypress/integration/features/woocommerce.cy.js
+++ tests/cypress/integration/features/woocommerce.cy.js
@@ -183,7 +183,9 @@ describe('WooCommerce Feature', { tags: '@slow' }, () => {
 				if (wcVersion === '6.4.0') {
 					cy.get('#place_order').click();
 				} else {
+					cy.intercept('/wp-json/wc/store/v1/checkout*').as('apiRequest');
 					cy.get('.wc-block-components-checkout-place-order-button').click();
+					cy.wait('@apiRequest');
 				}
 			});
 
